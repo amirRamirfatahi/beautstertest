01a51ec4e0d3466c3f72bb4b215ee6639c3aa56e
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-18 16:42:23 +0430

    Add otp functionality to User model

--- authenti/models/user.py
+++ authenti/models/user.py
@@ -1,13 +1,28 @@
+import string
+import random
+
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+
+from auth.constants import OTP_LENGTH
 
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
+    otp = models.CharField(max_length=OTP_LENGTH, blank=True, null=True)
 
     USERNAME_FIELD = 'username'
 
     def __str__(self):
         return f'user: {self.username}'
+
+    def generate_otp(self):
+        return ''.join(random.choices(string.digits, k=OTP_LENGTH))
+
+    def set_otp(self):
+        self.objects.filter(id=self.id).update(otp=otp)
+
+    def clear_otp(self):
+        self.objects.filter(id=self.id).update(otp=null)
 
