c188eb1da65c3ab7a7d593f7c374a79ccca66b67
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-19 14:12:39 +0430

    Add token field to User model
    Remove rest_framework.authtoken app from INSTALLED_APPS

--- authenti/models/user.py
+++ authenti/models/user.py
@@ -1,8 +1,11 @@
 import string
 import random
+import binascii
+import os
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from django.core.cache import cache
 
 from authenti.constants import OTP_LENGTH
 
@@ -11,6 +14,7 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     otp = models.CharField(max_length=OTP_LENGTH, blank=True, null=True)
+    token = models.CharField(max_length=40, unique=True, blank=True, null=True)
 
     USERNAME_FIELD = 'username'
 
@@ -21,8 +25,19 @@ class User(AbstractBaseUser):
         return ''.join(random.choices(string.digits, k=OTP_LENGTH))
 
     def set_otp(self):
-        self.objects.filter(id=self.id).update(otp=otp)
+        self.objects.filter(id=self.id).update(otp=self.generate_otp())
 
     def clear_otp(self):
         self.objects.filter(id=self.id).update(otp=None)
 
+    def generate_token(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def set_token(self):
+        token = self.generate_token()
+        cache.set(token, self.id)
+        self.objects.filter(id=self.id).update(token=token)
+
+    def clear_token(self):
+        cache.delete(self.token)
+        self.objects.filter(id=self.id).update(token=None)
