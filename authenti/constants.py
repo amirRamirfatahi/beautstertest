d31c14f4b0eacd8b6af6b6fdc96f3280e1801ec1
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-19 06:46:35 +0430

    Add send_otp_mail task and functionality

--- authenti/constants.py
+++ authenti/constants.py
@@ -1,2 +1,14 @@
+from django.conf import settings
+
 OTP_LENGTH = 6
 
+OTP_EMAIL_TEMPLATE = """
+dear {username},
+Following is your one time password:
+    {otp}
+"""
+
+OTP_SEND_MAIL_KWARGS = {
+    'subject': 'beautster otp',
+    'from_email': f'{{ settings.ADMIN_EMAIL }}'
+}
