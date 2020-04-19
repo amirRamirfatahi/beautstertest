b74fcbd7dbf1dd2aea6702bf01368c6ee6eb241e
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-19 14:13:43 +0430

    Add CustomTokenAuth authentication middleware

---
+++ middleware/authentication.py
@@ -0,0 +1,25 @@
+from django.core.cache import cache
+from django.contrib.auth import get_user_model
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class CustomTokenAuth(TokenAuthentication):
+    def get_model():
+        return get_user_model()
+
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+
+        cached_user_id = cache.get(key)
+        if cached_user_id:
+            user = model(id=cached_user_id)
+            return user, key
+
+
+        user = model.objects.filter(token=key).first()
+
+        if not user:
+            raise exceptions.AuthenticationFailed('Invalid token.')
+
+        return user, key
