cdbd930085b8b0f3b4d249d1796ec710f091176c
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-22 00:52:20 +0430

    Add booking to admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import Booking
+
+
+class BookingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Booking, BookingAdmin)
