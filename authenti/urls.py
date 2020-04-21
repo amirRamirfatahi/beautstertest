fa0f595749881a06fab936917744e2f6b288b110
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-21 12:39:24 +0430

    Add signin and verifyotp urls

--- authenti/urls.py
+++ authenti/urls.py
@@ -4,8 +4,7 @@ from authenti.api import views
 
 
 urlpatterns = [
-    path(r'signup/', views.signup_view, name='signup'),
-   # path(r'signin', views.signin_view, name='signin'),
-   # path(r'signout', views.signout_view, name='signout'),
-   # path(r'verifyotp', views.verifyotp_view, name='verifyotp')
+    path(r'signup', views.signup_view, name='signup'),
+    path(r'signin', views.signin_view, name='signin'),
+    path(r'verifyotp', views.verifyotp_view, name='verifyotp')
 ]
