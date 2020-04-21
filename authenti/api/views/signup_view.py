6a587f7b6989ed2036ce8e4b6696c92c5406d2f3
AmirReza <amir.r.amirfatahi@gmail.com>
2020-04-21 12:40:24 +0430

    Inherit SignupView from TemplateView to add GET

--- authenti/api/views/signup_view.py
+++ authenti/api/views/signup_view.py
@@ -1,13 +1,15 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
+from django.views.generic import TemplateView
 
 from authenti.api import serializers
 
 
-class SignupView(CreateAPIView):
+class SignupView(CreateAPIView, TemplateView):
     serializer_class = serializers.SignupSerializer
     permission_classes = [AllowAny]
+    template_name = 'signup.html'
 
 
 signup_view = SignupView.as_view()
